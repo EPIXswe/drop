658a8da7160a4a076dfd8ac92aaa293ce72aae57
EPIXswe <psegerfast@gmail.com>
2021-01-26 14:08:49 -0600

    updated project to LWJGL3

--- desktop/src/com/badlogic/drop/desktop/DesktopLauncher.java
+++ desktop/src/com/badlogic/drop/desktop/DesktopLauncher.java
@@ -1,15 +1,17 @@
 package com.badlogic.drop.desktop;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.badlogic.drop.Drop;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "Drop";
-		config.width = 800;
-		config.height = 480;
-		new LwjglApplication(new Drop(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle("Drop");
+		config.setWindowedMode(800, 480);
+		new Lwjgl3Application(new Drop(), config);
 	}
 }
